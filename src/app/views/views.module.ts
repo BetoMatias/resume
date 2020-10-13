734552285861d739670ef12c54451d3cfbb4c886
Humberto Matias <humberto.matiasf@gmail.com>
2020-10-13 03:00:32 -0300

    Borders and Side-Info

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ViewsRoutingModule } from './views-routing.module';
 import { OverviewComponent } from './overview/overview.component';
 import { PersonComponent } from './components/person/person.component';
+import { SideInfoComponent } from './components/side-info/side-info.component';
 
 
 @NgModule({
-  declarations: [OverviewComponent, PersonComponent],
+  declarations: [OverviewComponent, PersonComponent, SideInfoComponent],
   imports: [
     CommonModule,
     ViewsRoutingModule
