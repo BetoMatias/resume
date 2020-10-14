ffb0835c6c3376badd5da8bf4f0f4341dbf43aa0
Humberto Matias <humberto.matiasf@gmail.com>
2020-10-14 01:44:50 -0300

    Icons and Borders

--- src/app/views/components/side-info/side-info.component.ts
+++ src/app/views/components/side-info/side-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-side-info',
   templateUrl: './side-info.component.html',
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -6,12 +6,25 @@ import { OverviewComponent } from './overview/overview.component';
 import { PersonComponent } from './components/person/person.component';
 import { SideInfoComponent } from './components/side-info/side-info.component';
 
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+
+import { faIdCard } from '@fortawesome/free-solid-svg-icons';
+import { faUniversity } from '@fortawesome/free-solid-svg-icons';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+
 
 @NgModule({
   declarations: [OverviewComponent, PersonComponent, SideInfoComponent],
   imports: [
     CommonModule,
-    ViewsRoutingModule
+    ViewsRoutingModule,
+    FontAwesomeModule
   ]
 })
-export class ViewsModule { }
+export class ViewsModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faIdCard, faEnvelope, faUniversity, faGlobeAmericas, faBook);
+  }
+}
